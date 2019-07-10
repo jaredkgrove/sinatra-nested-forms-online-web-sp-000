b39a36a140c7af1c7a2c1da8d60386e76c8ac3ab
Jared Grove <jaredkgrove@gmail.com>
2019-07-10 15:52:52 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,6 +4,20 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get "/" do
+      erb :root
+    end
 
+    get "/new" do
+      erb :new
+    end
+
+    post "/show" do
+      @pirate = Pirate.new(params[:pirate])
+      params[:ships].each do |details|
+        Ship.new(details)
+      end
+
+    end
   end
 end
